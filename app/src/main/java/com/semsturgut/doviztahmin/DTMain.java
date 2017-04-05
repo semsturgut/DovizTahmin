3e55c2dda262f4ad021e2b4ac15cc419eaa92530
Şems <mtdetroit@gmail.com>
2017-04-05 23:08:26 +0300

    +Splash screen olusturuldu. +Icon olusturuldu.
    -Arayuz duzenlemeleri yapilacak.
    -Veri cekilecek sistem hazirlanacak.

---
+++ app/src/main/java/com/semsturgut/doviztahmin/DTMain.java
@@ -0,0 +1,13 @@
+package com.semsturgut.doviztahmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DTMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dtmain);
+    }
+}
